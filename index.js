348d80f75c5da95029fba6fd1d1fabf0f2587e17
Samandar <iwtuz166@gmail.com>
2023-08-05 21:21:42 +0500

    https to http

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const http = require('http');
-const https = require('https');
-const fs = require('fs');
 const router = require('./routes/router');
 
 const app = express();
@@ -16,26 +14,12 @@ const httpServer = http.createServer((req, res) => {
   res.end('Hello from your app!\n');
 });
 
-// HTTPS options
-const httpsOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/uis-team.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/uis-team.com/fullchain.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/uis-team.com/chain.pem')
-};
 
-// HTTPS server
-const httpsServer = https.createServer(httpsOptions, app);
+const httpPort = 8080;
 
-const httpPort = 80;
-const httpsPort = 443;
-
-// Start both HTTP and HTTPS servers
+// Start
 httpServer.listen(httpPort, () => {
   console.log(`HTTP Server running on port ${httpPort}`);
-});
-
-httpsServer.listen(httpsPort, () => {
-  console.log(`HTTPS Server running on port ${httpsPort}`);
 });
 
 // Error handling middleware

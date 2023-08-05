9bb06c4543fdf20b33de5b0e9e24ff6e13a6c90c
Samandar <iwtuz166@gmail.com>
2023-08-05 21:57:45 +0500

    add https

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const router = require('./routes/router');
 
 const app = express();
-const httpPort = 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/', router);
 
-// Error handling middleware
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/uis-team.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/uis-team.com/fullchain.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/uis-team.com/chain.pem')
+};
+
+const server = https.createServer(options, app);
+
+const port = 443; 
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 app.use((req, res, next) => {
   res.status(404).render('404');
-});
-
-// Start the Express server
-app.listen(httpPort, () => {
-  console.log(`Express server running on port ${httpPort}`);
 });

d2602bc7056e12603a49fc751d09b58127704216
Samandar <iwtuz166@gmail.com>
2023-08-05 21:25:11 +0500

    fix errors

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require('express');
-const http = require('http');
 const router = require('./routes/router');
 
 const app = express();
+const httpPort = 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/', router);
 
-// HTTP server
-const httpServer = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from your app!\n');
-});
-
-
-const httpPort = 8080;
-
-// Start
-httpServer.listen(httpPort, () => {
-  console.log(`HTTP Server running on port ${httpPort}`);
-});
-
 // Error handling middleware
 app.use((req, res, next) => {
   res.status(404).render('404');
+});
+
+// Start the Express server
+app.listen(httpPort, () => {
+  console.log(`Express server running on port ${httpPort}`);
 });
